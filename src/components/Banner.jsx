075fd1d1c07791d9d45576b4dd1ca8dda1478479
Arjav <1971arjavbarya@gmail.com>
2024-07-13 12:59:45 +0530

    changed image paths again

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,13 +3,12 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
-
       <div className="py-20 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
-      <div className="md:w-1/2">
-          <img src="/public/images/home/banner.png" />
+        <div className="md:w-1/2">
+          <img src="/images/home/banner.png" alt="Banner" />
           <div className="flex flex-col md:flex-row items-center justify-around -mt-14 gap-4">
             <div className="flex bg-white px-3 py-2 rounded-2xl items-center gap-3 shadow-md w-64">
-              <img src="/public/images/home/b-food1.png" className="rounded-2xl" />
+              <img src="/images/home/b-food1.png" className="rounded-2xl" alt="Spicy noodles" />
               <div className="space-y-1">
                 <div className="font-medium text-[#2C2C2C] mb-1">Spicy noodles</div>
                 <div className="rating rating-sm">
@@ -49,7 +48,7 @@ const Banner = () => {
               </div>
             </div>
             <div className="md:flex hidden bg-white px-3 py-2 rounded-2xl items-center gap-3 shadow-md w-64">
-              <img src="/public/images/home/b-food1.png" className="rounded-2xl" />
+              <img src="/images/home/b-food1.png" className="rounded-2xl" alt="Spicy noodles" />
               <div className="space-y-1">
                 <div className="font-medium text-[#2C2C2C] mb-1">Spicy noodles</div>
                 <div className="rating rating-sm">
@@ -96,14 +95,13 @@ const Banner = () => {
             <span className="text-green"> Food</span>{" "}
           </h2>
           <p className="text-xl text-[#4A4A4A]">
-            Where Each Plate Weaves a Story of Culinary Mastery ans Passionate
+            Where Each Plate Weaves a Story of Culinary Mastery and Passionate
             Craftsmanship
           </p>
           <button className="btn bg-green px-8 py-3 font-semibold text-white rounded-full">
             Order Now
           </button>
         </div>
-
       </div>
     </div>
   );
